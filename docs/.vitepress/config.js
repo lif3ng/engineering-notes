140d470a1595d5138f4189ed722bb031519897f9
lif3ng <lif3ng@icloud.com>
2021-02-25 22:35:48 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  lang: "zh-CN",
+  title: "工程化笔记",
+  description: "工程化碎碎念",
+  themeConfig: {
+    sidebar: {
+      "/": [
+        { text: "home", link: "index" },
+        { text: "打包", link: "bundle" },
+      ],
+    },
+  },
+};
